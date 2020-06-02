353ac86628a6d502957e819ef086138ed1d8a0d8
muhammadasadbastomi <tomycules@gmail.com>
2020-06-02 18:56:41 +0800

    crud method siswa controller

--- app/Http/Controllers/siswaController.php
+++ app/Http/Controllers/siswaController.php
@@ -2,17 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Kelas;
+use App\Siswa;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class siswaController extends Controller
 {
-    public function index(){
-
-        return view('admin.siswa.index');
+    public function index()
+    {
+        $data = Siswa::orderBy('id', 'desc')->get();
+        $kelas = Kelas::orderBy('nama_kelas')->get();
+        return view('admin.siswa.index', compact('data', 'kelas'));
     }
 
-    public function edit(){
+    public function store(Request $req)
+    {
+        $user = User::create($req->all());
 
-        return view('admin.siswa.edit');
+        $user->password = Hash::make($req->password);
+        if ($req->foto != null) {
+            $img = $req->file('foto');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('images/user', $foto);
+            $user->foto = $foto;
+        }
+        $user->update();
+
+        $siswa = $user->siswa()->create($req->all());
+
+        return redirect()->back()->withSuccess('Data berhasil disimpan');
+
+    }
+
+    public function show($uuid)
+    {
+        $data = Siswa::where('uuid', $uuid)->first();
+        return view('admin.siswa.show', compact('data'));
+    }
+
+    public function edit($uuid)
+    {
+        $data = Siswa::where('uuid', $uuid)->first();
+        $kelas = Kelas::orderBy('nama_kelas')->get();
+        return view('admin.siswa.edit', compact('data', 'kelas'));
+    }
+
+    public function update(Request $req, $uuid)
+    {
+        $userData = $req->except('password');
+        $siswa = siswa::where('uuid', $uuid)->first();
+        $user = user::where('id', $siswa->user_id)->first();
+        $user->fill($userData)->save();
+        if (isset($req->password)) {
+            $user->password = Hash::make($req->password);
+        } else {
+            $user->password = $user->password;
+        }
+
+        if ($req->foto != null) {
+            $img = $req->file('foto');
+            $FotoExt = $img->getClientOriginalExtension();
+            $FotoName = $user->id;
+            $foto = $FotoName . '.' . $FotoExt;
+            $img->move('images/user', $foto);
+            $user->foto = $foto;
+        } else {
+            $user->foto = $user->foto;
+        }
+
+        $user->update();
+
+        $siswa->fill($req->all())->save();
+        return redirect()->route('siswaIndex')->withSuccess('Data berhasil diubah');
+
+    }
+
+    public function destroy($uuid)
+    {
+        $data = siswa::where('uuid', $uuid)->first()->delete();
+        return redirect()->back()->withSuccess('Data berhasil dihapuse');
     }
 }
