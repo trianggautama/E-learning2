5bc531a22ac9d93bcc1961bb3ed3a6e481e85ac0
muhammadasadbastomi <tomycules@gmail.com>
2020-06-02 17:35:04 +0800

    backend kelas edit view

--- resources/views/admin/kelas/edit.blade.php
+++ resources/views/admin/kelas/edit.blade.php
@@ -1,51 +1,52 @@
 @extends('layouts.admin')
 
 @section('content')
-    <section role="main" class="content-body">
-		<header class="page-header">
-			<h2>Halaman Haul</h2>
-				<div class="right-wrapper text-right">
-					<ol class="breadcrumbs">
-						<li>
-						    <a href="#">
-								<i class="fas fa-home"></i>
-							</a>
-						</li>
-						<li><span>Edit Data Kelas</span></li>
-					</ol>
-				<a class="sidebar-right-toggle" ><i class="fas fa-chevron-left"></i></a>
-			</div>
-		</header>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-header">
-                        Edit Data Kelas
-                    </div>
-                    <div class="card-body">
-                        <form action="" enctype="multipart/form-data"  method="post">
-                            @csrf
-                            @method('PUT')                            
-                            <div class="form-group ">
-                                <label class="">Nama Kelas</label>
-                                <input type="text"class="form-control" name="kelas" id="kelas"  placeholder="ketua panitia" >
-                            </div>
-                    </div>
-                    <div class="card-footer text-right">
-                        <a href="{{Route('kelasIndex')}}" type="button" class="btn btn-default" >Batal</a>
-                        <button type="submit" class="btn btn-primary">ubah</button>
-                        </form>
-                    </div>
+<section role="main" class="content-body">
+    <header class="page-header">
+        <h2>Halaman Haul</h2>
+        <div class="right-wrapper text-right">
+            <ol class="breadcrumbs">
+                <li>
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                    </a>
+                </li>
+                <li><span>Edit Data Kelas</span></li>
+            </ol>
+            <a class="sidebar-right-toggle"><i class="fas fa-chevron-left"></i></a>
+        </div>
+    </header>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    Edit Data Kelas
+                </div>
+                <div class="card-body">
+                    <form action="" enctype="multipart/form-data" method="post">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group ">
+                            <label class="">Nama Kelas</label>
+                            <input type="text" class="form-control" name="nama_kelas" id="kelas"
+                                value="{{$data->nama_kelas}}" placeholder="ketua Kelas">
+                        </div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="{{Route('kelasIndex')}}" type="button" class="btn btn-default">Batal</a>
+                    <button type="submit" class="btn btn-primary">ubah</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
 @endsection
 @section('scripts')
-    <script>
-        $(document).ready(function() {
+<script>
+    $(document).ready(function() {
             $('#summernote').summernote();
         });
-    </script>
+</script>
 @endsection
