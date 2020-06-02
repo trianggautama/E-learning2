a30f281e975d8b1b60da77b778275c046384f4d8
muhammadasadbastomi <tomycules@gmail.com>
2020-06-02 18:55:36 +0800

    setup model siswa

---
+++ app/Siswa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use App\Traits\Uuid;
+use Illuminate\Database\Eloquent\Model;
+
+class Siswa extends Model
+{
+    use Uuid;
+
+    protected $fillable = [
+        'kelas_id', 'user_id', 'jk', 'tempat_lahir', 'tanggal_lahir', 'email', 'asal',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
