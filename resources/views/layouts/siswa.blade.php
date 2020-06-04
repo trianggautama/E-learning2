f9bd58d3d3e4614ccc14c928c4f0ff8017a7442d
tri angga utama <trianggategarutama@gmail.com>
2020-06-04 22:35:49 +0800

    tambahkan Auth nama

--- resources/views/layouts/siswa.blade.php
+++ resources/views/layouts/siswa.blade.php
@@ -80,8 +80,8 @@
 								data-lock-picture="img/!logged-user.jpg" />
 						</figure>
 						<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
-							<span class="name">John Doe Junior</span>
-							<span class="role">administrator</span>
+							<span class="name">{{Auth::user()->nama}}</span>
+							<span class="role">Siswa</span>
 						</div>
 						<i class="fa custom-caret"></i>
 					</a>
@@ -89,17 +89,9 @@
 						<ul class="list-unstyled mb-2">
 							<li class="divider"></li>
 							<li>
-								<a role="menuitem" tabindex="-1" href="pages-user-profile.html"><i
-										class="fas fa-user"></i> My Profile</a>
-							</li>
-							<li>
-								<a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i
-										class="fas fa-lock"></i> Lock Screen</a>
-							</li>
-							<li>
 								<a role="menuitem" tabindex="-1" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();"> <i class="fa fa-sign-out-alt"></i>
                                         {{ __('Logout') }}
                                     </a>
 
